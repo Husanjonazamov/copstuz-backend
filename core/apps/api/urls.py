d2c388c9c5991e507440a3bb2486af6069226e58
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-05-13 11:31:26 +0500

    urls: api yo'llarini qo'shildi views: Botuser uchun ModelViewSet

--- core/apps/api/urls.py
+++ core/apps/api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from core.apps.api.views import (
+    BotuserView, 
+    BranchView,
+    CategoryView
+)
 
 router = DefaultRouter()
-
+router.register(r"users", BotuserView, basename="botuser")
+router.register(r"category", CategoryView, basename="category")
+router.register(r"branch", BranchView, basename="branch")
 
 urlpatterns = [
     path("", include(router.urls)),
--- core/apps/api/views/botuser.py
+++ core/apps/api/views/botuser.py
@@ -1,14 +1,14 @@
 from django_core.mixins import BaseViewSetMixin
 from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from core.apps.api.models import BotuserModel
 from core.apps.api.serializers.botuser import CreateBotuserSerializer, ListBotuserSerializer, RetrieveBotuserSerializer
 
 
 @extend_schema(tags=["botuser"])
-class BotuserView(BaseViewSetMixin, ReadOnlyModelViewSet):
+class BotuserView(BaseViewSetMixin, ModelViewSet):
     queryset = BotuserModel.objects.all()
     serializer_class = ListBotuserSerializer
     permission_classes = [AllowAny]
